585162b7f85165a8b6db4eb4912268118b02b693
zzambbang <zzambbang@gmail.com>
2022-08-03 15:29:53 +0900

    0803 구간합4 소수찾기 배틀필드

---
+++ 구간합구하기/src/구간합구하기/Main.java
@@ -0,0 +1,33 @@
+package 구간합구하기;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception, IOException {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int M = in.nextInt();
+		
+		//n개의 숫자를 입력받아 배열에 넣기
+		int[] arr = new int[N + 1];
+		arr[0] = 0;
+		
+		//배열의 원소를 합해서 애초에 넣기
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + in.nextInt();
+		}
+		
+		//입력 받은 숫자의 범위만큼 배열에서 빼서 구함
+		for (int i = 0; i < M; i++) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			
+			System.out.println(arr[y] - arr[x - 1]);
+		}
+		
+	}
+}
---
+++ 구간합구하기5/src/구간합구하기5/Main.java
@@ -0,0 +1,10 @@
+package 구간합구하기5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
