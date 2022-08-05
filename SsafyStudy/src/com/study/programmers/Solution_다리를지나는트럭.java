4891198e192ce77f57bbd3e7e3549aa933224cd3
zzambbang <zzambbang@gmail.com>
2022-08-05 17:55:03 +0900

    0805 스터디

---
+++ SsafyStudy/src/com/study/programmers/Solution_다리를지나는트럭.java
@@ -0,0 +1,25 @@
+package com.study.programmers;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution_다리를지나는트럭 {
+	
+	public static void main(String[] args) {
+		//2	10	[7,4,5,6]	8
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weight = {7,4, 5, 6};
+		
+		System.out.println(solution(bridge_length, weight, truck_weight));
+	
+	}
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+//        if (queue)
+        
+        return answer;
+    }
+}
--- SsafyStudy/src/com/study/programmers/Solution_타겟넘버.java
+++ SsafyStudy/src/com/study/programmers/Solution_타겟넘버.java
@@ -1,3 +1,5 @@
+package com.study.programmers;
+
 
 import java.util.*;
 
