77bce3c8ac6c0b7649e77f4de02783a3449cc08a
zzambbang <zzambbang@gmail.com>
2022-08-10 09:16:17 +0900

    0809 수업시간 과제

--- 알고리즘풀이/src/com/ssafy/boj/DNA비밀번호_12891.java
+++ 알고리즘풀이/src/com/ssafy/boj/DNA비밀번호_12891.java
@@ -34,13 +34,14 @@ public class DNA비밀번호_12891 {
 			else if (init == 'T') current[3]++;
 		}
 		int cnt = 0;
+		int size = dna.length();
 		//처음저장한거 비교후 시작 
 		if(isAble(limit, current)) cnt++;
 		
 		//슬라이딩윈도우로 풀어볼게욥 ㅎ
 		int start = 0;
 		int end = start + P;
-		while (end < dna.length()) {
+		while (end < size) { //이것도변수로 빼는게 좋아 size로 ,, 매번 조회를 하니까
 
 			char out = dna.charAt(start);
 			char in = dna.charAt(end);
@@ -65,7 +66,7 @@ public class DNA비밀번호_12891 {
 		
 		
 	}
-	
+
 	public static boolean isAble(int[] limit, int[] current) {
 		for (int i = 0; i < 4; i++) {
 			if (limit[i] > current[i]) return false;
@@ -74,3 +75,8 @@ public class DNA비밀번호_12891 {
 	}
 
 }
+
+
+//if elseif 보다 switch로 처리하는게 더 빠르다.
+//switch는 비교하면서 내려가는게 아니라 더 좋아요 
+//그리고 비교가 많으니까 그냥 쌤처럼 메소드로 따로 빼는게 좋다.
