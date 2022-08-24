22f1fb765d55d5d237363877f63cf2ad5511b139
zzambbang <zzambbang@gmail.com>
2022-08-24 10:13:35 +0900

    0824 순조부넥퍼그래프탐색

---
+++ 온라인수업/src/com/ssafy/exercise/PermutationTest_0824.java
@@ -0,0 +1,44 @@
+package com.ssafy.exercise;
+import java.util.*;
+import java.io.*;
+
+public class PermutationTest_0824 {
+	static int N, R, totalCnt;
+	static int[] input, numbers;
+	static boolean[] isSelected;
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		R = Integer.parseInt(br.readLine());
+		totalCnt = 0;
+
+		input = new int[N];
+		numbers = new int[R];
+		isSelected = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+
+		perm(0);
+		System.out.println("총 경우의 수: " + totalCnt);
+	}
+
+	private static void perm(int cnt) {
+		if (cnt == R) {
+			totalCnt++;
+			System.out.println(Arrays.toString(numbers));
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (isSelected[i]) continue;
+			numbers[cnt] = input[i];
+			isSelected[i] = true;
+
+			perm(cnt + 1);
+			isSelected[i] = false;
+		}
+	}
+}
